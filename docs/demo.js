396cf90bf41720287ad6659f3dc64111eefbd705
Manuel Gil <mauelyeixon@gmail.com>
2018-02-07 19:04:04 -0400

    unused...

--- docs/demo.js
+++ docs/demo.js
@@ -1,12 +1,3 @@
-$.fn.extend({
-    animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
-    }
-});
-
 (function(){
 
 	$('.validate').click(function() {
